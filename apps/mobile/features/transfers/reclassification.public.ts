64d02a98d019a2bc480bbb1ea8569b47758ba1c9
obarbozaa <obarbozaa@gmail.com>
2026-05-15 00:17:57 -0500

    feat(mobile): reclassify transfers
    - Add pair reclassification
    
    - Preserve atomic rollback

---
+++ apps/mobile/features/transfers/reclassification.public.ts
@@ -0,0 +1,10 @@
+export {
+  type ReclassifyTransactionAsTransferError,
+  type ReclassifyTransactionAsTransferResult,
+  reclassifyTransactionAsTransfer,
+} from "./lib/reclassify-transaction-as-transfer";
+export {
+  type ReclassifyTransactionsAsTransferError,
+  type ReclassifyTransactionsAsTransferResult,
+  reclassifyTransactionsAsTransfer,
+} from "./lib/reclassify-transactions-as-transfer";
